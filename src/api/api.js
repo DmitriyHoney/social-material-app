eb7b163e0b1be0b02e61b8ca0e242cb5d58f5bd5
Dmitry Bolshakov <vanove2013@yandex.ru>
2020-06-07 23:50:46 +0300

    bug fixed

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        'API-KEY': '2aa95529-03f3-4e18-bb22-995bc7fdf5db'
+        'API-KEY': '718dc875-a290-4b03-a341-95d2eb3ffe02'
     }
 });
 
