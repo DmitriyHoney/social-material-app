0a69d541c3c6f2ff377e5b76a71de8fac0c83c6d
Dmitry Bolshakov <vanove2013@yandex.ru>
2020-06-07 23:37:40 +0300

    Updates

---
+++ precache-manifest.733f7a1cd1f20f51d5569edbf070b717.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e2b979db30d1cc29ccfdb6c7543302",
+    "url": "/social-material-app/index.html"
+  },
+  {
+    "revision": "8d222b2b723c2c25746d",
+    "url": "/social-material-app/static/css/main.8e4fae46.chunk.css"
+  },
+  {
+    "revision": "aa67f4c06e2dd328166a",
+    "url": "/social-material-app/static/js/2.ee3bfb1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-material-app/static/js/2.ee3bfb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d222b2b723c2c25746d",
+    "url": "/social-material-app/static/js/main.841e1f5e.chunk.js"
+  },
+  {
+    "revision": "a02cc52d6e4a6896f960",
+    "url": "/social-material-app/static/js/runtime-main.f04d025d.js"
+  },
+  {
+    "revision": "6843ac2928df80cbcca2a5e1685cb854",
+    "url": "/social-material-app/static/media/default-user.6843ac29.jpg"
+  },
+  {
+    "revision": "3a6ebddb3f958dacf957df60e1a23375",
+    "url": "/social-material-app/static/media/prelodader.3a6ebddb.svg"
+  }
+]);
